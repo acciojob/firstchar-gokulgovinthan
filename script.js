3a3f537ed7209c191d8d41b3060bc208b9e362a8
gokulgovinthan <goodgovinthan@gmail.com>
2024-08-13 09:01:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function firstChar(text) {
-  return text.charAt(0);
+  if(text)
+	return text.charAt(0);
+	else
+	  return ""
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-const text = prompt("Enter text:");
+//const text = prompt("Enter text:");
 alert(firstChar(text));
